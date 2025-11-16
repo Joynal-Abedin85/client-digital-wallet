0c9a0ad1edc3c9b77caa762cf37d732459f0b434
kazi tareq <kazitareq321@gmail.com>
2025-11-17 00:58:05 +0600

    add route

--- src/redux/baseapi.ts
+++ src/redux/baseapi.ts
@@ -5,7 +5,7 @@ import { fetchBaseQuery } from "@reduxjs/toolkit/query/react"
  export const baseApi = createApi({
     reducerPath: "baseApi",
       baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000",
+    baseUrl: "https://digital-wallet-backend-omega.vercel.app",
     prepareHeaders: (headers) => {
       const token = localStorage.getItem("token");
       if (token) {
